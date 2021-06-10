65933048198d6dbaab555fa2d599a1aef35f4bbb
Victoria-FZ-eng <vzulof@gmail.com>
2021-06-10 17:54:09 +0300

    p2

--- src/components/Footers/SimpleFooter.js
+++ src/components/Footers/SimpleFooter.js
@@ -38,7 +38,7 @@ class SimpleFooter extends React.Component {
             <Row className=" row-grid align-items-center mb-5">
               <Col lg="6">
                 <h3 className=" text-primary font-weight-light mb-2">
-                  Thank you for supporting us!
+                  Thank you for supporting !
                 </h3>
                 <h4 className=" mb-0 font-weight-light">
                   Let's get in touch on any of these platforms.
